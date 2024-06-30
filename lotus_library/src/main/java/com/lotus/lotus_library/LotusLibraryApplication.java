ca4e6f6c2f0072a31c5fb13cb3c45b683a3c86f5
Hoseop.Kim <kimho314@gmail.com>
2024-06-30 15:55:32 +0900

    feat : init project

---
+++ lotus_library/src/main/java/com/lotus/lotus_library/LotusLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.lotus.lotus_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotusLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotusLibraryApplication.class, args);
+    }
+
+}
---
+++ lotus_library/src/test/java/com/lotus/lotus_library/LotusLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotus.lotus_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotusLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
